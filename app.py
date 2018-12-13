b9046a630d30276f7b3737dce43fd81e927b221b
mjbutt <mjbutt@hotmail.co.uk>
2018-12-13 20:58:55 +0000

    initial conversion

--- app.py
+++ app.py
@@ -1,41 +1,24 @@
-from flask import Flask, request, jsonify
 from azure.storage.queue import QueueService
 import os
 import socket
 import subprocess
-
-app = Flask(__name__)
-
-@app.route('/', methods = ['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        html = "<h3>OGR says</h3>" \
-               "<code>{ogrresult}</code>"
-        return html.format(ogrresult=ogrresult())
-    if request.method == 'POST':
-        data = request.form # a multidict containing POST data
-        html = "<p>Received:</p>" \
-               "<code>{data}</code>" \
-               "<h3>OGR says</h3>" \
-               "<code>{ogrresult}</code>"
-        return html.format(ogrresult=ogrresult(), data=data)
 
 def pull_from_queue():
   queue_service = QueueService(account_name = 'adockertesting3306', account_key = '/8tgYKwEJO9DqTgvbQ/zI3zsvW3R8+rVwizEx4wi2ZkzuCmDjwMdHUPqHIiduGyGdH+D1ZygYQU4iQdN0TcMwQ==')
   queue_name = 'docker-queue-1'
   messages = queue_service.get_messages(queue_name)
   for message in messages:
-    print(message.content)
+    result = ogrresult()
+    print(result + " " + message.content)
   
 
 def ogrresult():
     try:	
       os.system("echo hello >> /mydata/file")
-      return subprocess.check_output(["ogrinfo", "/mydata/point.geojson"])
+      return subprocess.check_output(["ogr2ogr", "-f", "GPKG", "/mydata/output.gpkg", "/mydata/OSOpenGreenspace_SU.gml"])
     except subprocess.CalledProcessError as e:
       return e.output
       
 if __name__ == "__main__":
     pull_from_queue()
-    app.run(host='0.0.0.0', port=80)
     
